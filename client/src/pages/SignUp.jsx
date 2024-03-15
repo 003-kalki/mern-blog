d4d4387a60bd3f95afd925c119f442ab9c9f20e2
003-kalki <shyamcasm@gmail.com>
2024-03-15 05:34:53 +0530

    create signup ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,19 +1,19 @@
-import { Label, TextInput } from 'flowbite-react';
+import { Label, TextInput,Button } from 'flowbite-react';
 
 
 import {Link} from 'react-router-dom';
 export default function SignUp() {
   return (
     <div className='min-h-screen mt-20'>
-   <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center'>
+   <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
     {/* left*/}
-    <div className="">
+    <div className="flex-1">
     <Link   to ="/" className='self-center whitespace-nowrap  font-semibold dark:text-white text-4xl'>
                 <span  className='px-2 py-2 bg-gradient-to-r
                  from-orange-500             
                   to-orange-300
                   rounded-lg
-                  text-white' 
+                  text-white'   
                   
                 >Shyam 
                 </span>
@@ -21,14 +21,14 @@ export default function SignUp() {
             </Link>
            <p className='text-sm mt-5 font-semibold'>
 
-            ARTICLES ON   COMPUTER SCIENCE , HACKING AND PHILOSOPHY
+            ARTICLES ON TECHNOLOGY, HACKING AND PHILOSOPHY
            
              
            </p>
             </div>
 {/*right */}
-        <div className=''>
-        <form>
+        <div className='flex-1'>
+        <form className=''>
          <div >
          <Label value ='Your Username'/>
                 <TextInput
@@ -41,7 +41,7 @@ export default function SignUp() {
                <Label value ='Your Email'/>
                 <TextInput
                    type ='text'
-                   placeholder= 'Email'
+                   placeholder= 'name@company.com'
                    id ='email'/>
                   
                </div> 
@@ -53,7 +53,17 @@ export default function SignUp() {
                    id ='Password'/>
                   
                </div> 
+               <Button  className='mt-4 px-9 bg-gradient-to-r   from-orange-500 to            
+                  to-orange-300 ' type='submit '>
+                Sign Up 
+               </Button>
          </form>
+         <div className="flex gap-2 text-sm mt-5">
+          Have an account ?
+          <Link to='/sign-in' className='text-orange-500'>
+            Sign In
+          </Link>
+         </div>
 </div>
    </div>
     </div>
