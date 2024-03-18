36bfcddc0fdbc5ef01470edffe16e2c741696dac
003-kalki <shyamcasm@gmail.com>
2024-03-18 18:02:37 +0530

    add functionality to signup page

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
         required:true,
         unique:true,
     },
-    username:{
+    email:{
         type:String,
         required:true,
         unique:true,
